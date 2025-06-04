b242d80d3af0e463ba21f7466d931cc118bddc44
Mercy <19710443+MercurialPony@users.noreply.github.com>
2025-06-04 21:21:04 +0300

    Delete old files on boot, better object alter

--- USER_BOOT/PipUiPlusLoader.js
+++ USER_BOOT/PipUiPlusLoader.js
@@ -10,21 +10,59 @@ function perksSubmenu()
 	eval(fs.readFile("USER_BOOT/PIP_UI_PLUS/Perks.min.js"));
 }
 
-// deletes all properties in an object, renames, and re-inserts them while inserting new properties at specified points
-function alterObject(object, renameMap, propertyMap)
+
+
+function readdirSafely(path)
 {
+	try { return fs.readdir(path).filter(n => n != "." && n != ".."); }
+	catch (_) { return []; }
+}
+
+function deleteSafely(path)
+{
+	try { fs.unlink(path); }
+	catch (_) {};
+}
+
+function deleteRecursively(path)
+{
+	readdirSafely(path).forEach(n => deleteRecursively(path + "/" + n));
+	fs.unlink(path);
+};
+
+// Clean up the previous app files (this should be a temporary addition)
+deleteRecursively("USER_BOOT/PipUiPlus");
+deleteSafely("USER_BOOT/PipUiPlusLoader.js");
+
+
+// renames properties, adds new properties after specific properties, while maintaining their order in an object
+function alter(object, renames, properties)
+{
+	const propertiesAlreadyMoved = {};
+
 	for (const entry of Object.entries(object))
 	{
 		const key = entry[0];
 		const value = entry[1];
 
+		if (propertiesAlreadyMoved[key]) continue;
+
 		delete object[key];
-		object[renameMap[key] || key] = value;
-		Object.assign(object, propertyMap[key]);
+		object[renames[key] || key] = value;
+
+		const newProperties = properties[key];
+
+		for (const newKey in newProperties)
+		{
+			delete object[newKey];
+			object[newKey] = newProperties[newKey];
+
+			propertiesAlreadyMoved[newKey] = true;
+		}
 	}
 }
 
-alterObject(
+alter(
 	MODEINFO[MODE.STAT].submenu,
 	{ "CONNECT": "CONN", "DIAGNOSTICS": "DIAG" },
 	{ "STATUS": { "SPECIAL": specialSubmenu, "PERKS": perksSubmenu } },
